6d0561163dc7e68510ad197eb08bd8484659f594
Tuncay Kilicoglu <kilicoglutuncay@gmail.com>
2022-04-19 04:24:05 +0300

    update mock generation

--- mockgen.sh
+++ mockgen.sh
@@ -2,5 +2,5 @@ set -x
 
 rm -rf .mocks/*
 
-$GOPATH/bin/mockgen -destination=./.mocks/mock_url_handler.go -source=./handler/url.go -package=mocks &&
-$GOPATH/bin/mockgen -destination=./.mocks/mock_shortener_service.go -source=./service/shortener.go -package=mocks
+$GOPATH/bin/mockgen -destination=./.mocks/mock_url_handler.go -source=./internal/api/handler/url.go -package=mocks &&
+$GOPATH/bin/mockgen -destination=./.mocks/mock_shortener_service.go -source=./internal/api/service/shortener.go -package=mocks
